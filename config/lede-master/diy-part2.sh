a1efd4cc03b089ebaeded94c6cf6a121db7487a5
yoyoche1 <yoyoche1@163.com>
2024-12-03 01:58:43 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -29,8 +29,9 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 # ------------------------------- Other started -------------------------------
 #
 # Add luci-app-amlogic
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+svn co https://github.com/kenzok8/openwrt-packages package/kenzo 
+svn co https://github.com/kenzok8/small package/small 
 
 # Fix runc version error
 # rm -rf ./feeds/packages/utils/runc/Makefile
